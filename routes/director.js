07f41c68447d3f84b48a805548e308011d931093
lamnaourm <mohammed.lamnaour@gmail.com>
2024-02-29 08:41:48 +0100

    Insertion + Update + Delete

--- routes/director.js
+++ routes/director.js
@@ -47,11 +47,24 @@ routes.post('/add', (req, res) => {
 })
 
 routes.put('/update/:name', (req, res) => {
-    
+    const name = req.params.name;
+    const actor = req.body;
+
+    DirectorModel.updateOne({name:name}, actor).then(() => {
+        res.send(`${name} updated`)
+    }).catch((err) => {
+        res.send('Unable to update actor : ' + err)
+    })
 })
 
-routes.put('/delete/:name', (req, res) => {
-    
+routes.delete('/delete/:name', (req, res) => {
+    const name = req.params.name;
+
+    DirectorModel.deleteOne({name:name}).then(() => {
+        res.send(`${name} deleted`)
+    }).catch((err) => {
+        res.send('Unable to delete actor : ' + err)
+    })
 })
 
 
--- routes/movie.js
+++ routes/movie.js
@@ -38,11 +38,24 @@ routes.post('/add', (req, res) => {
 })
 
 routes.put('/update/:name', (req, res) => {
-    
+    const name = req.params.name;
+    const actor = req.body;
+
+    MovieModel.updateOne({name:name}, actor).then(() => {
+        res.send(`${name} updated`)
+    }).catch((err) => {
+        res.send('Unable to update actor : ' + err)
+    })
 })
 
-routes.put('/delete/:name', (req, res) => {
-    
+routes.delete('/delete/:name', (req, res) => {
+    const name = req.params.name;
+
+    MovieModel.deleteOne({name:name}).then(() => {
+        res.send(`${name} deleted`)
+    }).catch((err) => {
+        res.send('Unable to delete actor : ' + err)
+    })
 })
 
 
